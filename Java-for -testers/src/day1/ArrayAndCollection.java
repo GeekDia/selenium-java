ac3d5c4fc2c14d0c336a0a03f824e3a3795c0278
Mimi <mndiop.sne@gmail.com>
2019-11-18 17:03:18 +0000

    last commit day1 java for testers

---
+++ Java-for -testers/src/day1/ArrayAndCollection.java
@@ -0,0 +1,16 @@
+package day1;
+
+public class ArrayAndCollection {
+
+	public static void main(String[] args) {
+		
+		// Enhanced for loop for array and collection
+		
+		int array [][] = {{10,23},{45,56}};
+		
+			System.out.println("number of row" + array.length);
+		
+
+	}
+
+}
